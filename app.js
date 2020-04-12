3a2a9838abb137926962c9d8c9b7bfa8e677b2fe
Juan Hüttemann <juanfhuttemann@gmail.com>
2020-04-12 19:47:49 -0400

    add dev-data and public directory

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({message: "hello world"});
 });
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`starting server on ${port}`);
+  console.log(`
+  starting server on ${port}
+  go to http://localhost:3000
+  `);
 });
