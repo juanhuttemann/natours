de185d3542ccc29ac452dd6b6f4452d6750b002a
Juan Hüttemann <juanfhuttemann@gmail.com>
2020-04-13 19:13:46 -0400

    serving static file from public directory

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`))
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
