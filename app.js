50fcba52115ec6c553c95ba24b39d4daf162e117
Juan Hüttemann <juanfhuttemann@gmail.com>
2020-04-13 04:55:23 -0400

    add get tour route

--- app.js
+++ app.js
@@ -1,15 +1,66 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
 
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
 app.get("/api/v1/tours", (req, res) => {
-  res.status(200).json({message: "hello world"});
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.get("/api/v1/tours/:id", (req, res) => {
+  const id = req.params.id * 1
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    })
+  }
+
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour
+    }
+  });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
 app.listen(port, () => {
   console.log(`
   starting server on ${port}
-  go to http://localhost:3000
+  go to http://localhost:3000/api/v1/tours
   `);
 });
