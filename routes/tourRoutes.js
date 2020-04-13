dd88dcfc0a9943a5cc9fafaaea252b15a116a706
Juan Hüttemann <juanfhuttemann@gmail.com>
2020-04-13 18:54:24 -0400

    add checkBody middleware

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -8,11 +8,12 @@ const {
   updateTour,
   deleteTour,
   checkID,
+  checkBody
 } = require("../controllers/tourController");
 
 router.param("id", checkID);
 
-router.route("/").get(getAllTours).post(createTour);
+router.route("/").get(getAllTours).post(checkBody, createTour);
 
 router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
 
