1bf857cbdf93506a87331f5072835cc6dd2b8b13
Juan Hüttemann <juanfhuttemann@gmail.com>
2020-04-13 05:27:17 -0400

    add morgan as middleware

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const fs = require("fs");
+const morgan = require("morgan");
 
 const app = express();
 
+app.use(morgan("dev"));
 app.use(express.json());
 
 app.use((req, res, next) => {
