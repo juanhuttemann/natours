e90b2d809bcbb608bb1a41996442c23557e67f5c
Juan Hüttemann <juanfhuttemann@gmail.com>
2020-04-13 05:32:50 -0400

    add users routes

--- app.js
+++ app.js
@@ -92,6 +92,41 @@ const deleteTour = (req, res) => {
   });
 };
 
+const getAllUsers = (req, res)=>{
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined'
+  })
+}
+
+const getUser = (req, res)=>{
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined'
+  })
+}
+
+const createUser = (req, res)=>{
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined'
+  })
+}
+
+const updateUser = (req, res)=>{
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined'
+  })
+}
+
+const deleteUser = (req, res)=>{
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined'
+  })
+}
+
 app.route("/api/v1/tours").get(getAllTours).post(createTour);
 
 app
@@ -99,6 +134,15 @@ app
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+app.route("/api/v1/users").get(getAllUsers).post(createUser);
+
+app
+  .route("/api/v1/users/:id")
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
+
 
 const port = 3000;
 app.listen(port, () => {
