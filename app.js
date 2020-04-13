52fe145bf2744f16da56e987bb327ca8a94e05f2
Juan Hüttemann <juanfhuttemann@gmail.com>
2020-04-13 05:00:08 -0400

    add patch tours route

--- app.js
+++ app.js
@@ -57,6 +57,21 @@ app.post("/api/v1/tours", (req, res) => {
   );
 });
 
+app.patch("/api/v1/tours/:id", (req, res)=>{
+    if (req.params.id * 1 > tours.length) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Invalid ID",
+      });
+    }
+  res.status(200).json({
+    status: 'success',
+    data:{
+      tour: 'Updated tour...'
+    }
+  })
+})
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`
