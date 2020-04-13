51707aacb62bca11900742746fee0d8e6752311e
Juan Hüttemann <juanfhuttemann@gmail.com>
2020-04-13 05:59:40 -0400

    add server start script

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const tourRouter = require("./routes/tourRoutes");
-const userRouter = require("./routes/userRoutes")
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
@@ -16,10 +16,4 @@ app.use((req, res, next) => {
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`
-  starting server on ${port}
-  go to http://localhost:3000/api/v1/tours
-  `);
-});
+module.exports = app;
