236f8178ded2dce21b8fd5c8ce47ba143856f9b7
Juan Hüttemann <juanfhuttemann@gmail.com>
2020-04-15 02:07:41 -0400

    add config.env to .gitignore

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
 const app = require("./app");
+
+dotenv.config({ path: "./config.env" });
+
+const db = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATA_BASE_PASSWORD
+).replace("<USER>", process.env.DATA_BASE_USER);
+
+mongoose.connect(db, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+}).then(()=>{
+  console.log("db connection succesful!")
+})
+
 
 const port = process.env.PORT || 3000;
 
