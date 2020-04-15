db80cb3f82b0c9dbcd262ad1c35160e1339801f8
Juan Hüttemann <juanfhuttemann@gmail.com>
2020-04-15 02:22:29 -0400

    add Tour schema model

--- server.js
+++ server.js
@@ -9,13 +9,33 @@ const db = process.env.DATABASE.replace(
   process.env.DATA_BASE_PASSWORD
 ).replace("<USER>", process.env.DATA_BASE_USER);
 
-mongoose.connect(db, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-}).then(()=>{
-  console.log("db connection succesful!")
-})
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("db connection succesful!");
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, "A tour must have a name"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price"],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 
 const port = process.env.PORT || 3000;
